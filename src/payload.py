f432a5451bc1665fff725f545da0a7d57534c3b5
Anders Vaage <anders@unix.uib.no>
2018-08-31 15:58:53 +0200

    convert tuple to string

--- src/payload.py
+++ src/payload.py
@@ -1,6 +1,6 @@
 payload = {'ip': get_ipaddress(),
            'name': socket.gethostname(),
            'uptime': get_uptime(),
-           'kernel': os.uname(),
+           'kernel': ' '.join(os.uname()),
            'md5sum': checksum_file(__file__) }
 
